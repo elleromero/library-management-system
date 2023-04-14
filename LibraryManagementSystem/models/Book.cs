f3e7e783b579f7b5ccfa17840aa12dcbdcf7150c
omineko <>
2023-04-14 22:32:01 +0800

    added book model

---
+++ LibraryManagementSystem/models/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.models
+{
+    internal class Book
+    {
+        // TODO: Only generate and retrieve keys from the query. Remove primary and foreign keys later
+        public Book(string id, string categoryID, string title, string sypnosis, string author, string cover, string publisher, DateTime publicationDate, string isbn)
+        {
+            ID = id;
+            CategoryID = categoryID;
+            Title = title;
+            Sypnosis = sypnosis;
+            Author = author;
+            Cover = cover;
+            Publisher = publisher;
+            PublicationDate = publicationDate;
+            ISBN = isbn;
+        }
+
+        public string ID { get; set; }
+        public string CategoryID { get; set; }
+        public string Title { get; set; }
+        public string Sypnosis { get; set; }
+        public string Author { get; set; }
+        public string Cover { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string ISBN { get; set; }
+    }
+}
