3ac05472d5c0a17d2f50ab6ef011d1226bb809cf
omineko <>
2023-04-14 22:16:21 +0800

    added member model

---
+++ LibraryManagementSystem/models/Member.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.models
+{
+    internal class Member
+    {
+        // TODO: Only generate and retrieve keys from the query. Remove primary and foreign keys later
+        public Member(int id, string name, string address, string phone, string email = "")
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Phone = phone;
+            Email = email;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
