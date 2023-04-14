9c6f8d19c05315334431d48e8bde0880c3321393
omineko <>
2023-04-14 22:32:31 +0800

    added copy model

---
+++ LibraryManagementSystem/models/Copy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.models
+{
+    internal class Copy
+    {
+        // TODO: Only generate and retrieve keys from the query. Remove primary and foreign keys later
+        public Copy(string id, string bookID, string statusID)
+        {
+            Id = id;
+            BookID = bookID;
+            StatusID = statusID;
+        }
+
+        public string Id { get; set; }
+        public string BookID { get; set; }
+        public string StatusID { get; set; }
+    }
+}
