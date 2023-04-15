b76febe1b1caba00d2715dd7a90685ba35be1d65
omineko <>
2023-04-15 21:56:54 +0800

    added services

---
+++ LibraryManagementSystem/services/SetupService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.services
+{
+    internal class SetupService
+    {
+        // NOTE: This class will initialize the database when the program first loads
+        public SetupService() {
+            Console.WriteLine("HELLO TEST");
+        }
+    }
+}
