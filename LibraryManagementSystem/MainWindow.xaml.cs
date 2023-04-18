4715bbc3380790f02441ba89c2994b43758ac1f2
omineko <>
2023-04-18 21:35:37 +0800

    called setup service

--- LibraryManagementSystem/MainWindow.xaml.cs
+++ LibraryManagementSystem/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagementSystem.services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace LibraryManagementSystem
         public MainWindow()
         {
             InitializeComponent();
+
+            SetupService.Ready();
         }
     }
 }
