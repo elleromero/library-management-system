762b3d4f288a11de31f58937da1d36d8d75ccdb6
omineko <>
2023-04-18 19:35:43 +0800

    added SQLClient util

---
+++ LibraryManagementSystem/utils/SqlClient.cs
@@ -0,0 +1,28 @@
+﻿using LibraryManagementSystem.services;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.utils
+{
+    internal class SqlClient : EnvService
+    {
+        public static void Execute(Action<Exception?, SqlConnection?> callback, bool useBase = false)
+        {
+            SqlConnection conn = new SqlConnection(useBase ? GetConnBase() : GetConnStr());
+            Exception? error = null;
+
+            try { conn.Open(); }
+            catch (Exception e)
+            {
+                error = e;
+            }
+
+            callback(error, conn);
+            conn.Close();
+        }
+    }
+}
