4f5939faed2c79e068dbc64be6d26f34298c9313
omineko <>
2023-04-23 22:16:58 +0800

    updated interfaces

---
+++ LibraryManagementSystem/interfaces/IDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.interfaces
+{
+    internal interface IDAO<T>
+    {
+        ReturnResult<T> GetById(string id);
+        ReturnResultArr<T> GetAll();
+        ReturnResult<T> Update(T model);
+        ReturnResult<T> Create(T model);
+        bool Remove(string id);
+    }
+}
