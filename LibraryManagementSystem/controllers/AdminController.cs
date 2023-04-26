abd6b2558d6370e7debcf21a552f2b7819ebf918
omineko <>
2023-04-26 16:22:36 +0800

    added admin controller and dao

--- LibraryManagementSystem/controllers/AdminController.cs
+++ LibraryManagementSystem/controllers/AdminController.cs
@@ -1,8 +1,6 @@
 ﻿using Isopoh.Cryptography.Argon2;
 using LibraryManagementSystem.dao;
-using LibraryManagementSystem.interfaces;
 using LibraryManagementSystem.models;
-using LibraryManagementSystem.services;
 using LibraryManagementSystem.utils;
 using System;
 using System.Collections.Generic;
@@ -12,9 +10,9 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.controllers
 {
-    internal class AuthController : BaseController
+    internal class AdminController : BaseController
     {
-        public static ControllerModifyData<User> Register(
+        public static ControllerModifyData<User> CreateAdmin(
             string username,
             string password,
             string firstName,
@@ -22,7 +20,8 @@ namespace LibraryManagementSystem.controllers
             string address,
             string phone,
             string email = ""
-            ) {
+            )
+        {
             ControllerModifyData<User> returnData = new ControllerModifyData<User>();
             returnData.Result = default(User);
             Dictionary<string, string> errors = new Dictionary<string, string>();
@@ -47,11 +46,11 @@ namespace LibraryManagementSystem.controllers
                 "Password is too short"
                 );
 
-            // register user if theres no error
+            // register admin if theres no error
             if (errors.Count == 0)
             {
-                AuthDAO authDao = new AuthDAO();
-                ReturnResult<User> result = authDao.Create(new User
+                AdminDAO adminDao = new AdminDAO();
+                ReturnResult<User> result = adminDao.Create(new User
                 {
                     Username = username,
                     PasswordHash = Argon2.Hash(password), // This method consumes some time (2-10 secs.)
@@ -60,20 +59,17 @@ namespace LibraryManagementSystem.controllers
                         FirstName = firstName,
                         LastName = lastName,
                         Address = address,
-                        Phone = phone,
-                        Email = email
+                        Phone = phone
                     },
                     Role = new Role
                     {
-                        ID = 2
+                        ID = 1
                     }
                 });
 
                 isSuccess = result.IsSuccess;
                 if (isSuccess && result.Result != null)
                 {
-                    // clear password field
-                    result.Result.PasswordHash = "";
                     returnData.Result = result.Result;
                 }
             }
@@ -83,45 +79,83 @@ namespace LibraryManagementSystem.controllers
             return returnData;
         }
 
-        public static ControllerModifyData<User> SignIn(string username, string password)
+        public static ControllerModifyData<User> UpdateUser(
+            string userId,
+            string username,
+            string password,
+            string firstName,
+            string lastName,
+            string address,
+            string phone,
+            string email = ""
+            )
         {
             ControllerModifyData<User> returnData = new ControllerModifyData<User>();
             returnData.Result = default(User);
             Dictionary<string, string> errors = new Dictionary<string, string>();
             bool isSuccess = false;
 
-            // get username
-            AuthDAO authDao = new AuthDAO();
-            ReturnResult<User> result = authDao.GetByUsername(username);
+            // validate fields
+            if (!Validator.IsName(firstName)) errors.Add("first_name", "Name is invalid");
+            if (!Validator.IsName(lastName)) errors.Add("last_name", "Name is invalid");
+            if (string.IsNullOrWhiteSpace(address)) errors.Add("address", "Address is required");
+            if (string.IsNullOrWhiteSpace(phone)) errors.Add("phone", "Phone is required");
+            if (!string.IsNullOrWhiteSpace(email) && !Validator.IsEmail(email)) errors.Add("email", "Email is invalid");
+            if (!Validator.IsUsername(username)) errors.Add(
+                "username",
+                "Username should contain only letters, numbers, underscores, or hyphens"
+                );
+            if (!Validator.IsUsernameUnique(username)) errors.Add(
+                "username",
+                "Username already exists"
+                );
+            if (!Validator.IsPassword(password)) errors.Add(
+                "password",
+                "Password is too short"
+                );
 
-            // if username did not exist
-            if (result.Result != default(User))
+            // update user if theres no error
+            if (errors.Count == 0)
             {
-                // if password did not match
-                if (Argon2.Verify(result.Result.PasswordHash, password))
-                {
-                    // clear password field
-                    result.Result.PasswordHash = "";
+                AdminDAO adminDao = new AdminDAO();
 
-                    // both username and password matched
-                    AuthService.setSignedUser(result.Result);
-                    returnData.Result = result.Result;
-                    isSuccess = true;
+                // check if user with access exists
+                ReturnResult<User> user = adminDao.GetById(userId);
+
+                if (!user.IsSuccess)
+                {
+                    Console.WriteLine("THIS ONE");
+                    errors.Add("userId", "User not found");
+                    returnData.Errors = errors;
+                    returnData.IsSuccess = isSuccess;
+                    return returnData;
                 }
-                else errors.Add("password", "Incorrect password");
-            } else errors.Add("username", "Username did not exist");
+
+                // proceed if user is found
+                ReturnResult<User> result = adminDao.Update(new User
+                {
+                    ID = new Guid(userId),
+                    Username = username,
+                    PasswordHash = Argon2.Hash(password), // This method consumes some time (2-10 secs.)
+                    Member = new Member
+                    {
+                        FirstName = firstName,
+                        LastName = lastName,
+                        Address = address,
+                        Phone = phone,
+                        Email = email
+                    }
+                });
+
+                isSuccess = result.IsSuccess;
+                if (isSuccess && result.Result != null)
+                {
+                    returnData.Result = result.Result;
+                }
+            }
 
             returnData.Errors = errors;
             returnData.IsSuccess = isSuccess;
-            return returnData;
-        }
-
-        public static ControllerActionData LogOut() {
-            ControllerActionData returnData = new ControllerActionData();
-            
-            AuthService.setSignedUser(default(User));
-            returnData.IsSuccess = true;
-
             return returnData;
         }
     }
